78a0a744ac272c68944476fdc4ff4d0f86bd3e28
Irina Kagan <irinagamb@gmail.com>
2019-10-19 12:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Add your functions here
 
+<<<<<<< HEAD
 function map(src, func) {
   return src.map(func)
 }
@@ -11,4 +12,72 @@ function reduce(src, func, param) {
     return src.reduce(func)
   }
 
+=======
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
+  }
+  return r
+}
+
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
+  }
+  return r
+}
+
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+
+
+
+function map(mapToNegativize, mapToDouble, mapToNoChange, mapToSquare) {
+  
+})
+
+//function function map(a, b, c, d, scr){
+  //a(src)
+  //b(src)
+  //c(src)
+  //d(src)
+//}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+>>>>>>> 3e31c82076625dd5014eb53c5a9b8bc0fb87b684
 }
